aac6428a46cd2b5427e5769d0389ce08f01fa098
NaNiCatO <65011558@KMITL.AC.TH>
2023-12-03 03:35:51 +0700

    day1

---
+++ my-cv-internship/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Intro from './components/Intro';
+import Snow from './components/Snow';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Intro/>
+        <Snow/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-cv-internship/src/components/Snow.js
@@ -0,0 +1,72 @@
+
+import { motion } from 'framer-motion';
+
+
+const Unit = () => {
+  return (
+    <motion.div
+      style={{
+        position: 'fixed',
+        top: 100,
+        width: '1.5rem',
+        height: '1.5rem',
+        background: Math.random()*10 < 1 ? 'pink': 'whitesmoke', // 10% chance of pink snow
+        borderRadius: '50%',
+        zIndex: 0, // Set a negative value to move it to the back
+        opacity: 0.8 // Make it slightly transparent
+      }}
+      drag
+      // make snow fall from top of screen and random x position
+      initial={{ y: -200 ,x: (Math.random() * 1700)-850}}
+      // make snow fall '39.5rem' 630
+      animate={{ y: 630 }}
+      // snow falls forever and random delay and falling speed
+      transition={{ duration: (Math.random() * 4)+6, repeat: Infinity, repeatDelay: (Math.random()*5)+2 , delay: Math.random()*10}}
+      // make snow fall faster at the end
+      
+    />
+  );
+};
+
+const Snow = () => {
+  return (
+    <>
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+      <Unit />
+    </>
+  );
+};
+
+export default Snow;
