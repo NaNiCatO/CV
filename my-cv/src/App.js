7129184f2a7247e28cf6366f3c17887827ff568b
NaNiCatO <65011558@KMITL.AC.TH>
2023-12-08 16:45:21 +0700

    change

--- my-cv/src/App.js
+++ my-cv/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Welcome from './components/Welcome';
+import Snow from './components/Snow';
+import Personal from './components/Personal';
+import Education from './components/Education';
+import Project from './components/Project';
 
-function App() {
+
+
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Welcome />
+        <Snow />
       </header>
     </div>
   );
 }
 
-export default App;
+export function PersonalPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Personal />
+        <Snow />
+      </header>
+    </div>
+  );
+}
+
+export function EducationPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Education />
+      </header>
+    </div>
+  );
+}
+
+export function ProjectPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Project />
+        <Snow />
+      </header>
+    </div>
+  );
+}
+
+
--- my-cv/src/index.js
+++ my-cv/src/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App, PersonalPage, EducationPage, ProjectPage} from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<App />,
+  },
+  {
+      path: '/personal',
+      element: <PersonalPage />,
+  },
+  {
+      path: '/education',
+      element: <EducationPage />,
+  },
+  {
+      path: '/project',
+      element: <ProjectPage />,
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}/>
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
